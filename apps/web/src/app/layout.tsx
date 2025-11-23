bca1610198f826ba2cecbd4ffb6c19fb947e5e37
Celo Composer <support@celo.org>
2025-11-23 07:15:42 -0300

    welcome page

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-import { Navbar } from '@/components/navbar';
+// import { Navbar } from '@/components/navbar';
 import Providers from "@/components/providers"
 
 const inter = Inter({ subsets: ['latin'] });
@@ -49,7 +49,7 @@ export default function RootLayout({
         {/* Navbar is included on all pages */}
         <div className="relative flex min-h-screen flex-col">
           <Providers>
-            <Navbar />
+            {/* <Navbar /> */}
             <main className="flex-1">
               {children}
             </main>
